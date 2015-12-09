0f7c2da0c7ec4111c1b06b1a9e316deeb3ad52c8
Dieter Reuter <dieter.reuter@me.com>
2015-12-09 15:26:45 +0100

    Use tar.gz instead of tar.xz

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
+uname -a
+
 UBUNTU_ARCH="arm64"
 UBUNTU_RELEASE="15.10"
-IMAGE_FILE="ubuntu-${UBUNTU_RELEASE}-server-cloudimg-${UBUNTU_ARCH}-root.tar.xz"
+IMAGE_FILE="ubuntu-${UBUNTU_RELEASE}-server-cloudimg-${UBUNTU_ARCH}-root.tar.gz"
 
 wget "http://cloud-images.ubuntu.com/releases/${UBUNTU_RELEASE}/release/${IMAGE_FILE}"
 tar vtfz ${IMAGE_FILE}
+
+tree
 
 echo "OK"
