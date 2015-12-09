c1989109d3e542bce0cbbb81fd12e0a2aaea2a30
Dieter Reuter <dieter.reuter@me.com>
2015-12-09 15:20:55 +0100

    Add some build code

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
+UBUNTU_ARCH="arm64"
+UBUNTU_RELEASE="15.10"
+IMAGE_FILE="ubuntu-${UBUNTU_RELEASE}-server-cloudimg-${UBUNTU_ARCH}-root.tar.xz"
+
+wget "http://cloud-images.ubuntu.com/releases/${UBUNTU_RELEASE}/release/${IMAGE_FILE}"
+tar vtfz ${IMAGE_FILE}
+
 echo "OK"
